fbaa001ebc3976cd260fb175a2119b91890e0cb5
Sergei Poddubny <108150454+spoddub@users.noreply.github.com>
2022-07-24 02:47:36 +0300

    решенная задача

--- probe1.py
+++ probe1.py
@@ -1,8 +1,19 @@
-n = int(input())
-virus = 'anton'
-for i in range(1, n + 1):
-    s = input()
-    answer = ''
-    for j in virus:
-        if j in s:
-            
+# объявление функции
+def func(num1, num2):
+    s = 'делится'
+    s1 = 'не делится'
+    if num1 % num2 == 0:
+        return s
+    else:
+        return s1
+
+
+
+# считываем данные
+num1, num2 = int(input()), int(input())
+
+# вызываем функцию
+if func(num1, num2):
+    print(True)
+else:
+    print(False)
