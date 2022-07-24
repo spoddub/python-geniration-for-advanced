ca2fd7264a2f823b48b52bb04da76fe66a8986d7
Sergei Poddubny <108150454+spoddub@users.noreply.github.com>
2022-07-24 16:08:14 +0300

    решенная задача

--- probe1.py
+++ probe1.py
@@ -1,6 +1,9 @@
 list1 = [[1, 7, 8], [9, 7, 102], [102, 106, 105], [100, 99, 98, 103], [1, 2, 3]]
+total = 0
+counter = 0
+
 for i in list1:
-    i = i.reverse()
-
-
-print(list1)
+    for j in i:
+        total += i
+        counter += 1
+print(total / counter)
