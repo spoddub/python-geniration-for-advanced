a8b8c4a5105b886360f8ed5eb9fbf40ff2873329
Sergei Poddubny <108150454+spoddub@users.noreply.github.com>
2022-07-24 15:48:48 +0300

    решенная задача

--- probe1.py
+++ probe1.py
@@ -1,19 +1,3 @@
-# объявление функции
-def func(num1, num2):
-    s = 'делится'
-    s1 = 'не делится'
-    if num1 % num2 == 0:
-        return s
-    else:
-        return s1
-
-
-
-# считываем данные
-num1, num2 = int(input()), int(input())
-
-# вызываем функцию
-if func(num1, num2):
-    print(True)
-else:
-    print(False)
+list1 = [10, 20, [300, 400, [5000, 6000], 500], 30, 40]
+list1[2][2].append(7000)
+print(list1)
