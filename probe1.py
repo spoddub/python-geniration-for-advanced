ec768856e8a2a55aabfe03b7779cb3df6f21c97f
Sergei Poddubny <108150454+spoddub@users.noreply.github.com>
2022-07-24 15:59:31 +0300

    решенная задача

--- probe1.py
+++ probe1.py
@@ -1,4 +1,8 @@
-list1 = ['a', 'b', ['c', ['d', 'e', ['f', 'g'], 'k'], 'l'], 'm', 'n']
-sub_list = ['h', 'i', 'j']
-list1[2][1][2].extend(sub_list)
-print(list1)
+list1 = [[1, 7, 8], [9, 7, 102], [6, 106, 105], [100, 99, 98, 103], [1, 2, 3]]
+maximum = -1
+for i in list1:
+    if max(i) > maximum:
+        maximum = max(i)
+print(maximum)
+
+
