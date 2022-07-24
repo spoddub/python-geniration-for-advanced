ad5b9ba2de7098ba5b0327a406ef543673fe127f
Sergei Poddubny <108150454+spoddub@users.noreply.github.com>
2022-07-24 21:08:39 +0300

    решенная задача

--- probe1.py
+++ probe1.py
@@ -1,3 +1,11 @@
-import math
-
 n = int(input())
+P=[]
+for i in range(0,n):
+    row=[1]*(i+1)
+    for j in range(i+1):
+        if j!=0 and j!=i:
+            row[j]=P[i-1][j-1]+P[i-1][j]
+    P.append(row)
+
+for r in P:
+    print(*r)
