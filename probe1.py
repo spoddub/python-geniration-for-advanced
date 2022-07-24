c82f9d19e90148cd259c711f4b7dc93cdad7c84b
Sergei Poddubny <108150454+spoddub@users.noreply.github.com>
2022-07-24 21:16:50 +0300

    решенная задача

--- probe1.py
+++ probe1.py
@@ -1,11 +1,10 @@
-n = int(input())
-P=[]
-for i in range(0,n):
-    row=[1]*(i+1)
-    for j in range(i+1):
-        if j!=0 and j!=i:
-            row[j]=P[i-1][j-1]+P[i-1][j]
-    P.append(row)
-
-for r in P:
-    print(*r)
+a = input().split()
+s = []
+x = ''
+for i in a:    
+    if x != i:    
+        s.append(list(i))    
+    else:
+        s[-1].append(i)      
+    x = i    
+print(s)    
