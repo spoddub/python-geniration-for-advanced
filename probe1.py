b24bd1d554021f68643e40189a0c68e461510d49
Sergei Poddubny <108150454+spoddub@users.noreply.github.com>
2022-07-24 16:02:20 +0300

    решенная задача

--- probe1.py
+++ probe1.py
@@ -1,8 +1,6 @@
-list1 = [[1, 7, 8], [9, 7, 102], [6, 106, 105], [100, 99, 98, 103], [1, 2, 3]]
-maximum = -1
+list1 = [[1, 7, 8], [9, 7, 102], [102, 106, 105], [100, 99, 98, 103], [1, 2, 3]]
 for i in list1:
-    if max(i) > maximum:
-        maximum = max(i)
-print(maximum)
+    i = i.reverse()
 
 
+print(list1)
