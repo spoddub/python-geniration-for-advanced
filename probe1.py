13857b9cd5f25da3d90dc0c433eba7d8ca47297f
Sergei Poddubny <108150454+spoddub@users.noreply.github.com>
2022-07-27 14:23:56 +0300

    решенная задача

--- probe1.py
+++ probe1.py
@@ -3,9 +3,13 @@ matrix = []
 for i in range(n):
     temp = [int(num) for num in input().split()]
     matrix.append(temp)
-summa = 0
 
-for i in range(n):
-    summa += matrix[i][i]
+for i in matrix:
+    average = 0
+    summa = 0
+    for j in i:
+        average = sum(i) / len(i)
+        if average < int(j):
+            summa += 1
+    print(summa)
 
-print(summa)
