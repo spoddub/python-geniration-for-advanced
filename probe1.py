fbfc56ce05d9382120303256b6543b2fafc13f2d
Sergei Poddubny <108150454+spoddub@users.noreply.github.com>
2022-07-31 21:54:12 +0300

    решенная задача

--- probe1.py
+++ probe1.py
@@ -1,39 +1,14 @@
-n = int(input())
-matrix = []
-for i in range(n):
-    temp = [int(num) for num in input().split()]
-    matrix.append(temp)
+poets = [
+    ('Тургенев', 14),
+    ('Есенин', 13),
+    ('Маяковский', 28),
+    ('Фет', 15),
+    ('Лермонтов', 20)]
 
-for i in matrix:
-    average = 0
-    summa = 0
-    for j in i:
-        average = sum(i) / len(i)
-        if average < int(j):
-            summa += 1
-    print(summa)
+for i in range(len(poets)):
+    for j in range(i+1, len(poets)):
+        if poets[i] > poets[j]:
+            poets[i], poets[j] = poets[j], poets[i]
 
-
-n, m = map(int, input().split())
-matrix = []
-counter = 1
-
-for i in range(n):
-    matrix.append([])
-    for j in range(m):
-        matrix[i].append(counter)
-        counter += 1
-
-        print(f'{matrix[i][j]}'.ljust(3), end='')
-    print()
-
-    numbers = ((0, (9, 2)), (1, (4, 6, 3), (5, 2, 3), 8, 3))
-
-
-tuples = [(10, 20, 40), (40, 50, 60), (70, 80, 90), (10, 90), (1, 2, 3, 4), (5, 6, 10, 2, 1, 77)]
-new_tuples = list(tuples)
-for i in new_tuples:
-    i[-1] = 100
-
-
-print(new_tuples)
+print(poets[0])
+print(poets[-1])
