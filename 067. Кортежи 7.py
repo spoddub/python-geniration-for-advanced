0a71ea744f59fe9b69f11df08bd44c228c2122cf
Sergei Poddubny <108150454+spoddub@users.noreply.github.com>
2022-07-31 21:06:47 +0300

    решенная задача

---
+++ 067. Кортежи 7.py
@@ -0,0 +1,8 @@
+# Дополните приведенный код так, чтобы он вывел сумму минимального и максимального элементов кортежа numbers.
+
+
+numbers = (12.5, 3.1415, 2.718, 9.8, 1.414, 1.1618, 1.324)
+minimum_num = min(numbers)
+maximum_num = max(numbers)
+sum_num = minimum_num + maximum_num
+print(sum_num)
