bc171390827d9efa3ae0c0b7a5c00d3845ecb89a
Sergei Poddubny <108150454+spoddub@users.noreply.github.com>
2022-08-03 19:49:12 +0300

    решенная задача

---
+++ 108. Множества 9.py
@@ -0,0 +1,9 @@
+# Используя генератор множеств, дополните приведенный код так, что
+# бы он выбрал из списка files уникальные имена файлов c расширением .png,
+# независимо от регистра имен и расширений. Имена файлов вывести вместе с расширением,
+# все на одной строке, в нижнем регистре, в алфавитном порядке через пробел.
+
+
+files = ['python.png', 'qwerty.py', 'stepik.png', 'beegeek.org', 'windows.pnp', 'pen.txt', 'phone.py', 'book.txT', 'board.pNg', 'keyBoard.jpg', 'Python.PNg', 'apple.jpeg', 'png.png', 'input.tXt', 'split.pop', 'solution.Py', 'stepik.org', 'kotlin.ko', 'github.git']
+result = {c.lower() for c in files if c.lower().endswith('.png')}
+print(*sorted(result))
