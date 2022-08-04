a2cd2fbf7ca22b8d2632a1657f1f025126cca5b3
Sergei Poddubny <108150454+spoddub@users.noreply.github.com>
2022-08-05 00:33:49 +0300

    решенная задача

--- probe1.py
+++ probe1.py
@@ -160,5 +160,15 @@ print(*output)
 
 result = {}
 for i in range(1, 15):
-    result = result.setdefault(i, i * i)
+    result.setdefault(i, i * i)
 print(result)
+
+
+dict1 = {'a': 100, 'z': 333, 'b': 200, 'c': 300, 'd': 45, 'e': 98, 't': 76, 'q': 34, 'f': 90, 'm': 230}
+dict2 = {'a': 300, 'b': 200, 'd': 400, 't': 777, 'c': 12, 'p': 123, 'w': 111, 'z': 666}
+
+result = {}
+result = dict1.update(dict2)
+
+print(dict1)
+
