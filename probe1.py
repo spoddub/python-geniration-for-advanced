131950646c7aa0b0d1de665dafe46018cff12b04
Sergei Poddubny <108150454+spoddub@users.noreply.github.com>
2022-08-04 20:10:23 +0300

    решенная задача

--- probe1.py
+++ probe1.py
@@ -114,4 +114,12 @@ for mail in users:
 
 print(*sorted(names))
 
+numbers = {'0': 'zero', '1': 'one', '2': 'two', '3': 'three',
+           '4': 'four', '5': 'five', '6': 'six', '7': 'seven',
+           '8': 'eight', '9': 'nine'}
+letters = []
+number = str(input())
+for i in number:
+    letters.append(numbers[i])
+print(*letters)
 
