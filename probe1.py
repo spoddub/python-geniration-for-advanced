1a541c4efeb014654b16cdde2f0b8172aff86bbc
Sergei Poddubny <108150454+spoddub@users.noreply.github.com>
2022-08-06 13:31:46 +0300

    решенная задача

--- probe1.py
+++ probe1.py
@@ -1,23 +1,4 @@
-pets = [('Hatiko', 'Parker', 'Wilson', 50),
-        ('Rusty', 'Josh', 'King', 25),
-        ('Fido', 'John', 'Smith', 28),
-        ('Butch', 'Jake', 'Smirnoff', 18),
-        ('Odi', 'Emma', 'Wright', 18),
-        ('Balto', 'Josh', 'King', 25),
-        ('Barry', 'Josh', 'King', 25),
-        ('Snape', 'Hannah', 'Taylor', 40),
-        ('Horry', 'Martha', 'Robinson', 73),
-        ('Giro', 'Alex', 'Martinez', 65),
-        ('Zooma', 'Simon', 'Nevel', 32),
-        ('Lassie', 'Josh', 'King', 25),
-        ('Chase', 'Martha', 'Robinson', 73),
-        ('Ace', 'Martha', 'Williams', 38),
-        ('Rocky', 'Simon', 'Nevel', 32)]
-
-result = {}
-
-for pet, *name in pets:
-    result.setdefault(tuple(name), []).append(pet)
-
-print(result)
+lst = [word.strip('.,!?:;-') for word in input().lower().split()]
+for word in lst:
+    coun
 
