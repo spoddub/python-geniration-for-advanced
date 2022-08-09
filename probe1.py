4e0232ba3a7cc7b13e0e0199b28ba8c7feb9b697
Sergei Poddubny <108150454+spoddub@users.noreply.github.com>
2022-08-10 02:25:04 +0300

    решенная задача

--- probe1.py
+++ probe1.py
@@ -1,8 +1,11 @@
 import random
-n = int(input())
-for _ in range(n):
-    print(random.randint(1, 6))
 
-
-
-
+length = int(input())
+password = []
+for symbol in range(length):
+    num = random.randint(1, 2)
+    if num == 1:
+        password.append(chr(random.randint(65, 90)))
+    else:
+        password.append(chr(random.randint(97, 122)))
+print(*password, sep='')
