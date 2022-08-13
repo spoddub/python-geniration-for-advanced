e15832bfc0bbb346d24a17e4eb4de4f22f915712
Sergei Poddubny <108150454+spoddub@users.noreply.github.com>
2022-08-13 22:13:20 +0300

    решенная задача

--- probe1.py
+++ probe1.py
@@ -1,4 +1,12 @@
 from fractions import Fraction
 
-n, m = int(input()), int(input())
-print(Fraction(n, m))
+
+n, n2 = input(), input()
+summa = Fraction(n) + Fraction(n2)
+raznost = Fraction(n) - Fraction(n2)
+proizv = Fraction(n) * Fraction(n2)
+chastnoe = Fraction(n) / Fraction(n2)
+print(n, '+', n2, '=', summa)
+print(n, '-', n2, '=', raznost)
+print(n, '*', n2, '=', proizv)
+print(n, '/', n2, '=', chastnoe)
