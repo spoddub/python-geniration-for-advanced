95cac5caba085f42391622399ec57d95b9bc8241
Sergei Poddubny <108150454+spoddub@users.noreply.github.com>
2022-08-13 22:23:19 +0300

    решенная задача

--- probe1.py
+++ probe1.py
@@ -1,8 +1,10 @@
+from math import factorial
+
 from fractions import Fraction
 
 
 n = int(input())
 result = 0
 for i in range(1, n + 1):
-    result += Fraction(1, i ** 2)
+    result += Fraction(1, factorial(i))
 print(result)
