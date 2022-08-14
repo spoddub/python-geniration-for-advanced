cb09858c4aa4e69f40131e9c236f9d9ac4c7f2c7
Sergei Poddubny <108150454+spoddub@users.noreply.github.com>
2022-08-14 20:08:47 +0300

    решенная задача

--- probe1.py
+++ probe1.py
@@ -1,10 +1,7 @@
-from math import factorial
+def sq_sum(*args):
+    sp = []
+    for i in args:
+        sp.append(i ** 2)
+    return sum(sp)
 
-from fractions import Fraction
-
-
-n = int(input())
-result = 0
-for i in range(1, n + 1):
-    result += Fraction(1, factorial(i))
-print(result)
+print(sq_sum(1, 2, 3, 4, 5, 6, 7, 8, 9, 10))
