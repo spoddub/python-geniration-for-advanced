635c06db49588e34e3e637ea3f1c0782ac6a24a5
Sergei Poddubny <108150454+spoddub@users.noreply.github.com>
2022-08-14 20:17:52 +0300

    решенная задача

--- probe1.py
+++ probe1.py
@@ -1,7 +1,6 @@
-def sq_sum(*args):
+def mean(*args):
     sp = []
     for i in args:
-        sp.append(i ** 2)
-    return sum(sp)
-
-print(sq_sum(1, 2, 3, 4, 5, 6, 7, 8, 9, 10))
+        if type(i) in (int, float):
+            sp.append(i)
+    return sum(sp) / len(sp)
