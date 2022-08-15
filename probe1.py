f6ffbf7a3c3d2d880df5dec29047cc67ae7a1516
Sergei Poddubny <108150454+spoddub@users.noreply.github.com>
2022-08-15 20:40:08 +0300

    решенная задача

--- probe1.py
+++ probe1.py
@@ -1,8 +1,8 @@
-from math import sqrt
-points = [(-1, 1), (5, 6), (12, 0), (4, 3), (0, 1), (-3, 2), (0, 0), (-1, 3), (2, 0), (3, 0), (-9, 1), (3, 6), (8, 8)]
-def distance(a):
-    point = (a[0] ** 2 + a[1] ** 2)
-    point = sqrt(point)
-    return point
+numbers = [(10, 10, 10), (30, 45, 56), (81, 80, 39), (1, 2, 3), (12, 45, 67), (-2, -4, 100), (1, 2, 99), (89, 90, 34), (10, 20, 30), (50, 40, 50), (34, 78, 65), (-5, 90, -1)]
 
-print(sorted(points, key=distance))
+
+def sorting(tuple):
+    return min(tuple) + max(tuple)
+
+
+print(sorted(numbers, key=sorting))
