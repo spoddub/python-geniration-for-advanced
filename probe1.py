a07be593923193871ff89844f3b2319ef523f32f
Sergei Poddubny <108150454+spoddub@users.noreply.github.com>
2022-08-15 19:22:27 +0300

    решенная задача

--- probe1.py
+++ probe1.py
@@ -1,4 +1,5 @@
-def info_kwargs(**kwargs):
-    for key, value in sorted(kwargs.items()):
-        print(key + ':' + str(value))
-
+numbers = [(10, 10, 10), (30, 45, 56), (81, 39), (1, 2, 3), (12,), (-2, -4, 100), (1, 2, 99), (89, 9, 34), (10, 20, 30, -2), (50, 40, 50), (34, 78, 65), (-5, 90, -1, -5), (1, 2, 3, 4, 5, 6), (-9, 8, 4), (90, 1, -45, -21)]
+def average(x):
+    return sum(x) / len(x)
+print(min(numbers, key=average))
+print(max(numbers, key=average))
