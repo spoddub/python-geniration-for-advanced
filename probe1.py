c50b39d93491286201cfb7d5d390eafc3a876c44
Sergei Poddubny <108150454+spoddub@users.noreply.github.com>
2022-08-16 20:47:49 +0300

    решенная задача

--- probe1.py
+++ probe1.py
@@ -1,19 +1,27 @@
-def map(function, items):
+def map(items):
     result = []
     for item in items:
-        result.append(function(item))
+        result.append(filter(item))
     return result
 
-numbers = [3.56773, 5.57668, 4.00914, 56.24241, 9.01344, 32.12013, 23.22222, 90.09873, 45.45, 314.1528, 2.71828,
-           1.41546]
+
+def filter(items):
+    result = []
+    for item in items:
+        new_item = item ** 3
+        result.append(new_item)
+    return result
+
+def sorting(items):
+    result = []
+    for item in items:
+        if len(str(item)) == 3 and item % 5 == 2:
+            result.append(item)
+    return result
 
 
-def rounding(items):
-    res = []
-    for number in items:
-        res.append(round(number, 2))
-    return res
+numbers = [1014, 1321, 675, 1215, 56, 1386, 1385, 431, 1058, 486, 1434, 696, 1016, 1084, 424, 1189, 475, 95, 1434, 1462, 815, 776, 657, 1225, 912, 537, 1478, 1176, 544, 488, 668, 944, 207, 266, 1309, 1027, 257, 1374, 1289, 1155, 230, 866, 708, 144, 1434, 1163, 345, 394, 560, 338, 232, 182, 1438, 1127, 928, 1309, 98, 530, 1013, 898, 669, 105, 130, 1363, 947, 72, 1278, 166, 904, 349, 831, 1207, 1496, 370, 725, 926, 175, 959, 1282, 336, 1268, 351, 1439, 186, 273, 1008, 231, 138, 142, 433, 456, 1268, 1018, 1274, 387, 120, 340, 963, 832, 1127]
 
+new_numbers = map(numbers)
 
-print(map(rounding, numbers))
-
+print(*new_numbers, sep='\n')
