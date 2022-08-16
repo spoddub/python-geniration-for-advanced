cc61c3196d0a890c69544525662841d5d4ed823f
Sergei Poddubny <108150454+spoddub@users.noreply.github.com>
2022-08-16 04:32:56 +0300

    решенная задача

--- probe1.py
+++ probe1.py
@@ -1,12 +1,12 @@
-def sorting(x):
-    return x[number - 1]
+from math import *
+def resulting(number, word):
+    functions = {'квадрат': number ** 2,
+                 'куб': number ** 3,
+                 'корень': sqrt(number),
+                 'модуль': abs(number),
+                 'синус': sin(number)}
+    return functions[word]
 
 
-athletes = [('Дима', 10, 130, 35), ('Тимур', 11, 135, 39), ('Руслан', 9, 140, 33), ('Рустам', 10, 128, 30), ('Амир', 16, 170, 70), ('Рома', 16, 188, 100), ('Матвей', 17, 168, 68), ('Петя', 15, 190, 90)]
-
-number = int(input())
-
-athletes.sort(key=sorting)
-
-for athlete in athletes:
-    print(*athlete)
+num, wrd = int(input()), input().lower()
+print(resulting(num, wrd))
