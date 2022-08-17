afceeca21b759935cf7419d757b4638a97a00840
Sergei Poddubny <108150454+spoddub@users.noreply.github.com>
2022-08-18 02:46:34 +0300

    решенная задача

---
+++ 186. Функции высшего порядка 5.py
@@ -0,0 +1,8 @@
+# Напишите функцию func_apply(), принимающую на вход функцию и список значений и возвращающую список,
+# в котором каждое значение будет результатом применения переданной функции к переданному списку.
+
+def func_apply(function, items):
+    result = []
+    for item in items:
+        result.append(function(item))
+    return result
