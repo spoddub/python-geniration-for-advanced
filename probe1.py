21b2948e2772210072989cf7af36d410654c734a
Sergei Poddubny <108150454+spoddub@users.noreply.github.com>
2022-08-18 15:44:52 +0300

    решенная задача

--- probe1.py
+++ probe1.py
@@ -1,24 +1,14 @@
-def map(function, items):
-    result = []
-    for item in items:
-        result.append(function(item))
-    return result
+from functools import reduce 
 
+floats = [4.35, 6.09, 3.25, 9.77, 2.16, 8.88, 4.59, 34.23, 12.12, 4.67, 2.45, 9.32]
+words = ['racecar', 'akinremi', 'deed', 'temidayo', 'omoseun', 'civic', 'TATTARRATTAT', 'malayalam', 'nun']
+numbers = [4, 6, 9, 23, 5]
 
-def filtering(items):
-    result = []
-    for item in items:
-        if 9 < abs(item) < 100 and item % 7 == 0:
-            result.append(item)
-    return result
+# Исправьте этот код
+map_result = list(map(lambda num: round(num ** 2, 1), floats))
+filter_result = list(filter(lambda name: name == name[::-1] and len(name) > 4, words))
+reduce_result = reduce(lambda num1, num2: num1 * num2, numbers, 1)
 
-def mult(items):
-    result = []
-    for item in items:
-        result.append(item ** 2)
-    return result
-
-
-numbers = [77, 293, 28, 242, 213, 285, 71, 286, 144, 276, 61, 298, 280, 214, 156, 227, 228, 51, -4, 202, 58, 99, 270, 219, 94, 253, 53, 235, 9, 158, 49, 183, 166, 205, 183, 266, 180, 6, 279, 200, 208, 231, 178, 201, 260, -35, 152, 115, 79, 284, 181, 92, 286, 98, 271, 259, 258, 196, -8, 43, 2, 128, 143, 43, 297, 229, 60, 254, -9, 5, 187, 220, -8, 111, 285, 5, 263, 187, 192, -9, 268, -9, 23, 71, 135, 7, -161, 65, 135, 29, 148, 242, 33, 35, 211, 5, 161, 46, 159, 23, 169, 23, 172, 184, -7, 228, 129, 274, 73, 197, 272, 54, 278, 26, 280, 13, 171, 2, 79, -2, 183, 10, 236, 276, 4, 29, -10, 41, 269, 94, 279, 129, 39, 92, -63, 263, 219, 57, 18, 236, 291, 234, 10, 250, 0, 64, 172, 216, 30, 15, 229, 205, 123, -105]
-
-print(sum(mult(filtering(numbers))))
+print(map_result)
+print(filter_result)
+print(reduce_result)
