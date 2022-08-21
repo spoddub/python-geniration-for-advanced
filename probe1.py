cddd34e6b4a887a91390dd990b2851c7e02eaefe
Sergei Poddubny <108150454+spoddub@users.noreply.github.com>
2022-08-21 03:50:25 +0300

    решенная задача

--- probe1.py
+++ probe1.py
@@ -1,12 +1,13 @@
-from decimal import *
+is_num = lambda number: number.replace('.', '', 1) .isdecimal()
 
-is_non_negative_num = lambda number: number.isdigit()
+print(is_num('10.34ab'))
+print(is_num('10.45'))
+print(is_num('-18'))
+print(is_num('-34.67'))
+print(is_num('987'))
+print(is_num('abcd'))
+print(is_num('123.122.12'))         
+print(is_num('-123.122'))
+print(is_num('--13.2'))
 
-print(is_non_negative_num('10.34ab'))
-print(is_non_negative_num('10.45'))
-print(is_non_negative_num('-18'))
-print(is_non_negative_num('-34.67'))
-print(is_non_negative_num('987'))
-print(is_non_negative_num('abcd'))
-print(is_non_negative_num('123.122.12'))
-print(is_non_negative_num('123.122'))
+
