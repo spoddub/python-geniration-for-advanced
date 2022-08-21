4040dbc3b3637627550265734f7c65f1cd91a924
Sergei Poddubny <108150454+spoddub@users.noreply.github.com>
2022-08-21 04:26:30 +0300

    решенная задача

--- probe1.py
+++ probe1.py
@@ -1,4 +1,7 @@
-data = [(19542209, 'New York'), (4887871, 'Alabama'), (1420491, 'Hawaii'), (626299, 'Vermont'), (1805832, 'West Virginia'), (39865590, 'California'), (11799448, 'Ohio'), (10711908, 'Georgia'), (10077331, 'Michigan'), (10439388, 'Virginia'), (7705281, 'Washington'), (7151502, 'Arizona'), (7029917, 'Massachusetts'), (6910840, 'Tennessee')]
+data = ['год', 'человек', 'время', 'дело', 'жизнь', 'день', 'рука', 'раз', 'работа', 'слово', 'место', 'лицо', 'друг', 'глаз', 'вопрос', 'дом', 'сторона', 'страна', 'мир', 'случай', 'голова', 'ребенок', 'сила', 'конец', 'вид', 'система', 'часть', 'город', 'отношение', 'женщина', 'деньги']
 
-for i in sorted(data, key=lambda x: x[1][-1], reverse=True):
-    print(f"{i[1]}: {i[0]}")
+sorted_data = sorted(data)
+
+new_data = sorted(sorted_data, key=lambda word: len(word))
+
+print(*new_data)
