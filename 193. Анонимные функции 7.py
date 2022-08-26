94aeec0825f9a6cb2897512c3d7ddf0168f19b95
Sergei Poddubny <108150454+spoddub@users.noreply.github.com>
2022-08-26 03:38:15 +0300

    решенная задача

--- 193. Анонимные функции 7.py
+++ 193. Анонимные функции 7.py
@@ -5,4 +5,3 @@
 words = ['beverage', 'monday', 'abroad', 'bias', 'abuse', 'abolish', 'abuse', 'abuse', 'bid', 'wednesday', 'able', 'betray', 'accident', 'abduct', 'bigot', 'bet', 'abandon', 'besides', 'access', 'friday', 'bestow', 'abound', 'absent', 'beware', 'abundant', 'abnormal', 'aboard', 'about', 'accelerate', 'abort', 'thursday', 'tuesday', 'sunday', 'berth', 'beyond', 'benevolent', 'abate', 'abide', 'bicycle', 'beside', 'accept', 'berry', 'bewilder', 'abrupt', 'saturday', 'accessory', 'absorb']
 
 print(*sorted(filter(lambda word: len(word) == 6, words)))
-
