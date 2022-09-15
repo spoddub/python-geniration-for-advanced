57f7772312f9647cf7a33c13b703e393a02fc75d
Sergei Poddubny <108150454+spoddub@users.noreply.github.com>
2022-09-15 15:58:29 +0300

    решенная задача

--- probe1.py
+++ probe1.py
@@ -1,6 +1,5 @@
-countries = ['Russia', 'USA', 'UK', 'Germany', 'France', 'India']
-capitals = ['Moscow', 'Washington', 'London', 'Berlin', 'Paris', 'Delhi']
-population = [145_934_462, 331_002_651, 80_345_321, 67_886_011, 65_273_511, 1_380_004_385]
+from random import randint
 
-for country in zip(capitals, countries, population):
-    print(country[0], 'is the capital of', str(country[1]) + ',', 'population equal', country[2], 'people.')
+with open('random.txt', 'w') as file:
+    arr = [str(randint(111, 777)) + '\n' for i in range(25)]
+    file.writelines(arr)
